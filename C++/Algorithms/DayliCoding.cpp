997540a3c0c6845e31ff5ee7183a0a558104ac82
tuyenlt <keplerlu@gmail.com>
2022-12-09 21:04:26 +0700

    update

--- C++/Algorithms/DayliCoding.cpp
+++ C++/Algorithms/DayliCoding.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <unordered_map>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -27,7 +28,48 @@ void SecondMaxReNum(){
     }
     cout << max2;
 }
+//...............................
+struct MAXVAL{
+    int val;
+    bool found = false;
+};
 
+int a[101][101];
+MAXVAL maxPath[100][100];
+
+int maxPoint(int n,int i,int j){
+    int res;
+    if(maxPath[i][j].found == false){
+        if(j == n) res = a[i][j];
+        else if(i == 0) res = a[i][j] + max(maxPoint(n,i,j++),maxPoint(n,i++,j++));
+        else if(i == n) res = a[i][j] + max(maxPoint(n,i,j++),maxPoint(n,i--,j++));
+        else res = a[i][j] + max(maxPoint(n,i++,j++), max(maxPoint(n,i,j++),maxPoint(n,i--,j++)));
+        maxPath[i][j].val = res;
+        maxPath[i][j].found = true;
+        return res;
+
+    }else return maxPath[i][j].val; 
+}
+
+
+void MaximumPath(){
+    int n;
+    cin >> n;
+    for(int i = 1; i<n+1; i++){
+        for(int j = 1; j<n+1;j++){
+            cin >> a[i][j];
+        }
+    }
+    //......................
+    int res = INT8_MIN;
+    int temp;
+    for(int i = 0; i<n+1;i++){
+        temp = maxPoint(n,i,1);
+        if(temp > res) res = temp;
+    }   
+    cout << temp;
+}
+//..........................................................
 
 
 bool DragonFight(int MyPower,multimap<int,int> Dra){
